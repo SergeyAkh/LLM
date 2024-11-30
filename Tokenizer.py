7d33ddce5ad081d5051767546f8097df4b8679f2
SergeyAkh <akhmadulinsergey@gmail.com>
2024-11-30 22:13:30 +0200

    new code

--- Tokenizer.py
+++ Tokenizer.py
@@ -1,4 +1,7 @@
 import re
+from torch.utils.data import Dataset, DataLoader
+import torch
+
 
 class SimpleTokenizerV1:
     def __init__(self, vocab):
@@ -8,9 +11,10 @@ class SimpleTokenizerV1:
     def encode(self, text):
         preprocessed = re.split(r'([,.:?!_"()\']|--|\s)', text)
 
-        preprocessed = [
-            item.strip() for item in preprocessed if item.strip()
-        ]
+
+        preprocessed = [item.strip() for item in preprocessed if item.strip()]
+        preprocessed = [item if item in self.str_to_int else "<|unk|>" for item in preprocessed]
+
         ids = [self.str_to_int[s] for s in preprocessed]
         return ids
     def decode(self, ids):
@@ -18,3 +22,23 @@ class SimpleTokenizerV1:
 
         text = re.sub(r'\s+([,.?!"()\'])', r'\1', text)
         return text
+
+
+class GPTDatasetV1(Dataset):
+    def __init__(self,txt, tokenizer, max_length, stride):
+        self.input_ids = []
+        self.target_ids = []
+
+        token_ids = tokenizer.encode(txt, allowed_special={"<|endoftext|>"})
+
+        for i in range(0, len(token_ids) - max_length, stride):
+            input_chunk = token_ids[i:i+max_length]
+            target_chunk = token_ids[i+1:i+max_length+1]
+            self.input_ids.append(torch.tensor(input_chunk))
+            self.target_ids.append(torch.tensor(target_chunk))
+
+    def __len__(self):
+        return len(self.input_ids)
+
+    def __getitem__(self,idx):
+        return self.input_ids[idx], self.target_ids[idx]
